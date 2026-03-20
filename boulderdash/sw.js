78ffc54de21c8b6ce4718c0b0564660bb6a907bb
Marek Bakalarczuk <marek.bakalarczuk@gmail.com>
2026-03-20 11:51:35 +0100

    new version

--- boulderdash/sw.js
+++ boulderdash/sw.js
@@ -22,7 +22,13 @@ self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    ).then(() => {
+      return self.clients.matchAll().then(clients => {
+        clients.forEach(client => {
+          client.postMessage({ type: 'reload' });
+        });
+      });
+    })
   );
   self.clients.claim();
 });
